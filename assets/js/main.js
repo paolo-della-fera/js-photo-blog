932f423a3106346d3043e0cfe6e2cfbe6ab03c8e
paolo-della-fera <paolodf2005@gmail.com>
2026-02-25 15:12:48 +0100

    add html structure and api endpoint setup

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+/* 
+
+<div class="col">
+    <div class="card">
+        <img class="pin" src="./assets/img/pin.svg" alt="">
+
+        <img src="https://marcolanci.it/boolean/assets/pictures/1.png" class="card-img-top" alt="...">
+
+        <div class="card-body">
+            <p class="card-text">date</p>
+            <h2 class="card-title fw-bold">Title</h2>
+        </div>
+    </div>
+</div>
+
+*/
+
+
+
+
+// take the API and save in a variable 
+const endpoint = 'https://lanciweb.github.io/demo/api/pictures/'
+
+
+// take the DOM node 
+const rowEl = document.querySelector('.row');
+// console.log(endpoint, rowEl);
